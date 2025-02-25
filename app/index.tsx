ac08b0c734cc03ce6099b0cba0432f7049d49859
Kazilsky <mk9151341600@gmail.com>
2025-02-25 21:18:47 +0300

    Сводка ветки для разделения по коммитам

--- app/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-  // Редиректим на страницу входа
-  return <Redirect href="/(auth)" />;
-}
--- constants/userInfo.ts
+++
@@ -1,4 +0,0 @@
-export const testStorageUser = {
-  username: "test",
-  password: "test2"
-}
